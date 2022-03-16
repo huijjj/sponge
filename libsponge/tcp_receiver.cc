a788216015672776b8fe2edb190fdb3bf0b1ca14
huijjj <hui0213@postech.ac.kr>
2022-03-16 10:49:05 +0000

    add: TCPReceiver

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -11,9 +11,36 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    const TCPHeader hdr = seg.header();
+
+    if(hdr.syn) {
+        _isn.emplace(hdr.seqno);
+    }
+
+    if(_isn.has_value()) {
+        size_t abs_ack = _reassembler.stream_out().bytes_written() + 1;
+        size_t abs_seq = unwrap(hdr.seqno, _isn.value(), abs_ack);
+        if(hdr.syn) {
+            _reassembler.push_substring(seg.payload().copy(), abs_seq, hdr.fin);
+        }
+        else {
+            _reassembler.push_substring(seg.payload().copy(), abs_seq - 1, hdr.fin);
+        }
+    }
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const { 
+    if(_isn.has_value()) {
+        size_t abs_ack = _reassembler.stream_out().bytes_written(); // count of bytes written to buffer
+        abs_ack++; // 1 byte for SYN
+        if(_reassembler.stream_out().input_ended()) {
+            abs_ack++; // 1byte for FIN
+        }
+        return wrap(abs_ack, _isn.value());
+    }
+    else {
+        return {}; 
+    }
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const { return _reassembler.stream_out().remaining_capacity(); }
